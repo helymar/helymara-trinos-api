571e256a5423134968f846938ca6040a18465406
Helymar <helymar@outlook.com>
2021-11-18 19:02:05 -0300

    fix: relation

--- src/database/migrations/20210825204340-create-tweet.js
+++ src/database/migrations/20210825204340-create-tweet.js
@@ -19,7 +19,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         references: {
-          model: 'user',
+          model: 'User',
           key: 'id'
       },
       },
