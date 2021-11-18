8bad6e43b4520a57a356dec0c7c31fcde6fc189a
Helymar <helymar@outlook.com>
2021-11-18 18:11:11 -0300

    fix: comments alias

--- src/controllers/tweets.js
+++ src/controllers/tweets.js
@@ -43,7 +43,7 @@ const getAllMyTweets = async (req, res, next) => {
     const where = {
       userId: req.user.id,
     };
-    const myTweets = await Tweets.findAll({ where, ...req.pagination, include: [{ model: Comments, attributes: ['id', 'text', 'likeCounter', 'tweetId', 'createdAt', 'updatedAt'], as: 'coments' }] });
+    const myTweets = await Tweets.findAll({ where, ...req.pagination, include: [{ model: Comments, attributes: ['id', 'text', 'likeCounter', 'tweetId', 'createdAt', 'updatedAt'], as: 'comments' }] });
 
     res.json(new TweetsSerializer(myTweets, await req.getPaginationInfo(Tweets)));
   } catch (err) {
